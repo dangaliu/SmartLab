376b6856ad42390e9301be37e0946e4064ab9617
dangaliu <ii4603399@gmail.com>
2023-03-09 17:55:32 +0300

    SwipeControlTouchListener created

---
+++ app/src/main/java/com/example/smartlab/utils/SwipeControlTouchListener.kt
@@ -0,0 +1,54 @@
+package com.example.smartlab.utils
+
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+
+enum class SwipeDirection {
+    ALL, // swipe allowed in left and right both directions
+    LEFT, // swipe allowed in only Left direction
+    RIGHT, // only right
+    NONE // swipe is disabled completely
+}
+
+class SwipeControlTouchListener: RecyclerView.OnItemTouchListener  {
+    private var initialXValue = 0f
+    private var direction: SwipeDirection = SwipeDirection.ALL
+
+    fun setSwipeDirection(direction: SwipeDirection) {
+        this.direction = direction
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, event: MotionEvent): Boolean {
+        return !isSwipeAllowed(event)
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+
+    private fun isSwipeAllowed(event: MotionEvent): Boolean {
+        if (direction === SwipeDirection.ALL) return true
+        if (direction == SwipeDirection.NONE) //disable any swipe
+            return false
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            initialXValue = event.x
+            return true
+        }
+        if (event.action == MotionEvent.ACTION_MOVE) {
+            try {
+                val diffX: Float = event.x - initialXValue
+                if (diffX > 0 && direction == SwipeDirection.RIGHT) {
+                    // swipe from left to right detected
+                    return false
+                } else if (diffX < 0 && direction == SwipeDirection.LEFT) {
+                    // swipe from right to left detected
+                    return false
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
+        return true
+    }
+
+}
--- app/src/main/java/com/example/smartlab/view/fragments/OnboardingFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/OnboardingFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.smartlab.databinding.FragmentOnboardingBinding
+import com.example.smartlab.utils.SwipeControlTouchListener
+import com.example.smartlab.utils.SwipeDirection
 import com.example.smartlab.view.adapters.OnboardingAdapter
 import com.example.smartlab.viewmodel.OnboardingViewModel
 
@@ -31,6 +35,11 @@ class OnboardingFragment : Fragment() {
 
     private fun initOnboardingViewPager() {
         binding.vpOnboarding.adapter = OnboardingAdapter(viewModel.onboardingItems)
+        (binding.vpOnboarding[0] as RecyclerView).addOnItemTouchListener(SwipeControlTouchListener().apply {
+            setSwipeDirection(
+                SwipeDirection.RIGHT
+            )
+        })
         binding.circleIndicator.setViewPager(binding.vpOnboarding)
     }
 }
