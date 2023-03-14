8adfb2dd40797f8429f354f5e7a9ce114e405553
dangaliu <ii4603399@gmail.com>
2023-03-14 13:26:21 +0300

    validate email edit text fix

--- app/src/main/java/com/example/smartlab/utils/Utils.kt
+++ app/src/main/java/com/example/smartlab/utils/Utils.kt
@@ -3,5 +3,13 @@ package com.example.smartlab.utils
 import android.util.Patterns
 
 fun isEmailValid(email: String): Boolean {
-    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    val pattern = "[a-z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-z0-9][a-z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-z0-9][a-z0-9\\-]{0,25}" +
+            ")+"
+    val regex = Regex(pattern)
+    return regex.matches(email)
 }
