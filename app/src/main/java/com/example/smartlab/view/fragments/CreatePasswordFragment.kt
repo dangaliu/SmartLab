50fb955a62359ae9d1c0ab095c72afaa38459927
dangaliu <ii4603399@gmail.com>
2023-03-16 22:23:13 +0300

    implemented password realization

--- app/src/main/java/com/example/smartlab/view/fragments/CreatePasswordFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/CreatePasswordFragment.kt
@@ -1,15 +1,22 @@
 package com.example.smartlab.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.iterator
 import androidx.fragment.app.Fragment
+import com.example.smartlab.R
 import com.example.smartlab.databinding.FragmentCreatePasswordBinding
 
-class CreatePasswordFragment: Fragment() {
+class CreatePasswordFragment : Fragment() {
 
     private lateinit var binding: FragmentCreatePasswordBinding
+
+    private val TAG = this::class.java.simpleName
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,4 +28,62 @@ class CreatePasswordFragment: Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val code = StringBuilder(4)
+        binding.gridLayoutPasswordKeyboard.iterator().forEach { child ->
+            if (child is TextView) {
+                child.setOnClickListener {
+                    if (code.length < 4) {
+                        code.append(child.text)
+                        updatePasswordIndicators(code.length)
+                        Log.d(TAG, "setListeners: code - $code")
+                    }
+                }
+            }
+            if (child is ImageView) {
+                child.setOnClickListener {
+                    if (code.isNotEmpty()) {
+                        code.deleteCharAt(code.lastIndex)
+                        Log.d(TAG, "setListeners: code - $code")
+                        updatePasswordIndicators(code.length)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updatePasswordIndicators(codeLength: Int) {
+        clearIndicators()
+        when(codeLength) {
+            1 -> {
+                binding.ivPassword1.setImageResource(R.drawable.selected_indicator)
+            }
+            2 -> {
+                binding.ivPassword1.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword2.setImageResource(R.drawable.selected_indicator)
+            }
+            3 -> {
+                binding.ivPassword1.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword2.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword3.setImageResource(R.drawable.selected_indicator)
+            }
+            4 -> {
+                binding.ivPassword1.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword2.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword3.setImageResource(R.drawable.selected_indicator)
+                binding.ivPassword4.setImageResource(R.drawable.selected_indicator)
+            }
+        }
+    }
+
+    private fun clearIndicators() {
+        binding.passwordIndicatorsContainer.iterator().forEach { child ->
+            (child as ImageView).setImageResource(R.drawable.unselected_indicator)
+        }
+    }
 }
