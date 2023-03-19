339e9cde68fffad0aae82617886eea722858dbe3
dangaliu <ii4603399@gmail.com>
2023-03-19 23:04:25 +0300

    minor changes

--- app/src/main/java/com/example/smartlab/view/fragments/PatientCardFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/PatientCardFragment.kt
@@ -1,10 +1,15 @@
 package com.example.smartlab.view.fragments
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.view.iterator
 import androidx.fragment.app.Fragment
 import com.example.smartlab.R
 import com.example.smartlab.databinding.FragmentPatientCardBinding
@@ -25,6 +30,7 @@ class PatientCardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListeners()
+        setTextWatchersToEditTextInLayout()
     }
 
     private fun setListeners() {
@@ -46,5 +52,48 @@ class PatientCardFragment : Fragment() {
             }
         }
         popUpMenu.show()
+    }
+
+    private fun setTextWatchersToEditTextInLayout() {
+        binding.mainContainer.iterator().forEach {
+            if (it is EditText) {
+                (it as EditText).addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int
+                    ) {
+                    }
+
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int
+                    ) {
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        if (checkAllEditText()) {
+                            binding.btnCreatePatientCard.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.accent, null))
+                            binding.btnCreatePatientCard.isEnabled = true
+                        } else {
+                            binding.btnCreatePatientCard.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.accent_inactive, null))
+                            binding.btnCreatePatientCard.isEnabled = false
+                        }
+                    }
+
+                })
+            }
+        }
+    }
+
+    private fun checkAllEditText(): Boolean {
+        return with(binding) {
+            etName.text.isNotBlank() && etMiddleName.text.isNotBlank() &&
+                    etSurname.text.isNotBlank() && etDateOfBirth.text.isNotBlank() &&
+                    etGender.text.isNotBlank()
+        }
     }
 }
