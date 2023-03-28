22c2fec1e9c9058e3176c88c9ae62d160893eb2a
dangaliu <ii4603399@gmail.com>
2023-03-28 10:51:41 +0300

    cart screen base created

--- app/src/main/java/com/example/smartlab/view/fragments/AnalyzesFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/AnalyzesFragment.kt
@@ -11,6 +11,7 @@ import androidx.core.view.isEmpty
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartlab.R
@@ -102,6 +103,9 @@ class AnalyzesFragment : Fragment() {
         }
         binding.tvCancel.setOnClickListener {
             binding.etSearch.clearFocus()
+        }
+        binding.btnGoToCart.setOnClickListener {
+            findNavController().navigate(R.id.action_analyzesFragment_to_cartFragment)
         }
     }
 
--- app/src/main/java/com/example/smartlab/view/fragments/CartFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/CartFragment.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartlab.databinding.FragmentCartBinding
+import com.example.smartlab.view.adapters.CartAdapter
 import com.example.smartlab.viewmodel.CartViewModel
 
 class CartFragment : Fragment() {
 
     private lateinit var binding: FragmentCartBinding
     private val viewModel: CartViewModel by viewModels()
+
+    private lateinit var cartAdapter: CartAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +26,34 @@ class CartFragment : Fragment() {
     ): View? {
         binding = FragmentCartBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+        setObservers()
+        initCartRecyclerView()
+    }
+
+    private fun setListeners() {
+        binding.ivBtnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.items.observe(viewLifecycleOwner) { items ->
+            val cartItems = items.filter { it.isInCard }
+            cartAdapter.updateItems(cartItems)
+        }
+    }
+
+    private fun initCartRecyclerView() {
+        cartAdapter = CartAdapter(requireContext(), listOf())
+        binding.rvCartItems.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = cartAdapter
+        }
     }
 
 
