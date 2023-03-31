66f111eccc93e99920a8d291d41b0aad00348944
dangaliu <ii4603399@gmail.com>
2023-03-31 23:52:11 +0300

    fix

--- app/src/main/java/com/example/smartlab/view/fragments/WaitingFragment.kt
+++ app/src/main/java/com/example/smartlab/view/fragments/WaitingFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.smartlab.databinding.FragmentWaitingBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class WaitingFragment: Fragment() {
+class WaitingFragment : Fragment() {
 
     private lateinit var binding: FragmentWaitingBinding
 
@@ -18,5 +21,14 @@ class WaitingFragment: Fragment() {
     ): View? {
         binding = FragmentWaitingBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(3000)
+            binding.tvWaiting.text = "Производим оплату..."
+            delay(2000)
+        }
     }
 }
