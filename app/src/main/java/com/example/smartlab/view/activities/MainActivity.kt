1b0e37ae5739b935c973e96c0006ffb5448edc9a
dangaliu <ii4603399@gmail.com>
2023-04-01 16:34:48 +0300

    fix

--- app/src/main/java/com/example/smartlab/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/view/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.smartlab.view.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.cartFragment, R.id.orderFragment, R.id.waitingFragment -> hideBottomNavigation()
+                else -> showBottomNavigation()
+            }
+        }
+    }
+
+    private fun showBottomNavigation() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNavigation() {
+        binding.bottomNavigationView.visibility = View.GONE
     }
 }
